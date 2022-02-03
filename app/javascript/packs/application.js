26080b67b4d1b07fdef7d3ba85958830382f9e4d
Ekokurniadi <ekokurniadi.02@gmail.com>
2022-02-03 16:43:59 +0700

    fix version of bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,10 +16,11 @@ require("channels")
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 import 'bootstrap';
-import '../stylesheets/application';
 
 document.addEventListener("turbolinks:load", () => {
     $('[data-toggle="tooltip"]').tooltip()
 });
+
+import '../stylesheets/application';
 require('admin-lte');
 import "@fortawesome/fontawesome-free/js/all";
